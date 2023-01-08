817cbc7669e89de5fcae58e9a1af27e65b2e138d
MarcinCzernek <mustang.mcz@gmail.com>
2023-01-08 12:38:07 +0100

    Choosing one second working fine

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-
+const minCounter = document.getElementById('mincounter');
 const counter = document.getElementById('counter');
 const button = document.getElementById('switch');
+const okLength =document.getElementById('oklength');
+const lenInfo = document.getElementById('lenInfo');
 
-var seconds = 0;
+let minutes = 0;
+var seconds = 60;
 var interval ;
 let going = false;
 let length = 25;
-const okLength =document.getElementById('oklength');
-const lenInfo = document.getElementById('lenInfo');
+
 lenInfo.innerHTML = `Długość trwania pomodoro: ${length} minut`;
 
 okLength.onclick = function(){
@@ -15,21 +17,52 @@ okLength.onclick = function(){
     lenInfo.innerHTML = `Długość trwania pomodoro: ${length} minut`;
 }
 
-let beat = new Audio('bell.mp3');
+let beat = new Audio('materials/bell.mp3');
 
-button.onclick = function(){
-    if (!going){
-        seconds = length*60 || 0;
-        interval = setInterval(function() {
-            document.body.style.background = "seagreen";
-            counter.innerHTML = `Pozostałe sekundy: ${seconds}`;
-            seconds--;
-            if(seconds <= 0){
-                clearInterval(interval);
-                document.body.style.background = "steelblue";
-                beat.play();        }   },1000)
-        counter.innerHTML = `Pozostałe sekundy: ${seconds}`;
-        button.innerHTML = 'STOP';
+button.onclick = function() {
+    if (!going) {
+        minutes = length - 1;
+        interval = setInterval(function () {
+            if (minutes > 0) {
+                seconds--;
+                document.body.style.background = "seagreen";
+                counter.innerHTML = `${seconds}`;
+                minCounter.innerHTML = `${minutes}`;
+                button.innerHTML = 'STOP';
+                if (seconds == 0 && minutes > 0) {
+                    minutes--;
+                    if(minutes > 0){
+                        seconds = 60;
+                    }
+                    minCounter.innerHTML = `${minutes}`;
+                }if(minutes == 0 ){
+                    minutes = length;
+                    seconds = 60;
+                    clearInterval(interval);
+                    document.body.style.background = "steelblue";
+                    button.innerHTML = "START";
+                    beat.play();
+                    minCounter.innerHTML = `${minutes}`;
+                    counter.innerHTML = `${seconds}`;
+                }
+            }else if (minutes == 0 && seconds > 0){
+                seconds--;
+                document.body.style.background = "seagreen";
+                counter.innerHTML = `${seconds}`;
+                minCounter.innerHTML = `${minutes}`;
+                button.innerHTML = 'STOP';
+                if(seconds == 0){
+                    minutes = length;
+                    seconds = 60;
+                    clearInterval(interval);
+                    document.body.style.background = "steelblue";
+                    button.innerHTML = "START";
+                    beat.play();
+                    minCounter.innerHTML = `${minutes}`;
+                    counter.innerHTML = `${seconds}`;
+                }
+            }
+        }, 1000)
     }else{
         clearInterval(interval);
         button.innerHTML = "START";
@@ -37,3 +70,4 @@ button.onclick = function(){
     }
     going = !going;
 };
+

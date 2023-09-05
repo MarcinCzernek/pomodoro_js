1cd420ba6fed400b73974f972eff291229923409
Marcin Czernek <63116220+MarcinCzernek@users.noreply.github.com>
2023-09-05 19:21:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
 const minCounter = document.getElementById('mincounter');
 const counter = document.getElementById('counter');
-const button = document.getElementById('switch');
-const okLength =document.getElementById('oklength');
-const lenInfo = document.getElementById('lenInfo');
+const startStopPomodoro = document.getElementById('switch');
+const okButton =document.getElementById('okButton');
+const lengthInfoText = document.getElementById('lengthInfoText');
 
 let minutes = 0;
 var seconds = 60;
 var interval ;
-let going = false;
-let length = 25;
-
-lenInfo.innerHTML = `Długość trwania pomodoro: ${length} minut`;
-
-okLength.onclick = function(){
-    length =  document.getElementById('length').value;
-    lenInfo.innerHTML = `Długość trwania pomodoro: ${length} minut`;
-}
+let pomodoroGoing = false;
+let pomodoroLength = 25;
 
 let beat = new Audio('materials/bell.mp3');
 
-button.onclick = function() {
-    if (!going) {
-        minutes = length - 1;
-        interval = setInterval(function () {
-            if (minutes > 0) {
-                seconds--;
-                document.body.style.background = "seagreen";
-                counter.innerHTML = `${seconds}`;
-                minCounter.innerHTML = `${minutes}`;
-                button.innerHTML = 'STOP';
-                if (seconds == 0 && minutes > 0) {
-                    minutes--;
-                    if(minutes > 0){
-                        seconds = 60;
-                        console.log('minutes > 0');
-                    }
-                    console.log('seconds == 0 && minutes > 0');
-                    minCounter.innerHTML = `${minutes}`;
-                }if(minutes == 0){
-                    console.log(seconds)
-                    seconds = 60
-                    document.body.style.background = "steelblue";
-                    button.innerHTML = "START";
-                    beat.play();
-                    minCounter.innerHTML = `00`;
-                    counter.innerHTML = `00`;
-                    clearInterval(interval);
-                    console.log('minutes == 0');
-                }
-            }
-        }, 1000)
-    }else{
-        clearInterval(interval);
-        button.innerHTML = "START";
-        document.body.style.background = "steelblue";
-    }
-    going = !going;
-};
+lengthInfoText.innerHTML = `Długość trwania pomodoro: ${pomodoroLength} minut`;
+
+okButton.onclick = updateLength;
+
+function updateLength (){
+  pomodoroLength = document.getElementById('length').value;
+  lengthInfoText.innerHTML = `Długość trwania pomodoro: ${pomodoroLength} minut`;
+}
+
+function activatePomodoro() {
+  if (!pomodoroGoing) {
+    minutes = pomodoroLength - 1;
+    seconds = 60; 
+    interval = setInterval(function () {
+      if (minutes >= 0) {
+        seconds--;
+        document.body.style.background = "seagreen";
+        counter.innerHTML = seconds < 10 ? `0${seconds}` : seconds.toString();
+        minCounter.innerHTML = minutes < 10 ? `0${minutes}` : minutes.toString();
+        startStopPomodoro.innerHTML = 'STOP';
+
+        if (seconds === 0 && minutes > 0) {
+          minutes--;
+          seconds = 60; 
+          minCounter.innerHTML = minutes < 10 ? `0${minutes}` : minutes.toString();
+        }
+
+        if (minutes === 0 && seconds === 0) {
+          document.body.style.background = "steelblue";
+          startStopPomodoro.innerHTML = "START";
+          beat.play();
+          minCounter.innerHTML = `00`;
+          counter.innerHTML = `00`;
+          clearInterval(interval);
+        }
+      }
+    }, 1000);
+  } else {
+    clearInterval(interval);
+    startStopPomodoro.innerHTML = "START";
+    document.body.style.background = "steelblue";
+  }
+  pomodoroGoing = !pomodoroGoing;
+}
+
+startStopPomodoro.onclick = activatePomodoro;
+  
 
